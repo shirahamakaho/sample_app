4d94821be5b46ae7fbd9bcc3b6d4304a04b4e5e4
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-06 01:41:23 +0000

    [finish]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show',as:'list'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
+  get 'lists/:id/edit' => 'lists#edit',as:'edit_list'
+  patch 'lists/:id' => 'lists#update',as:'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
